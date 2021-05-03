44d8bf50c9c57c97d5b561fc4a9e10be3d1b93f1
August <auggierutter@gmail.com>
2021-05-03 14:01:40 -0400

    commit tree 98e07b0a6da8662c2e1929012dde7490146e2cd0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+
+  return `This Saturday, I want to ${activity}!` 
+}
+
+const mondayWork = function (activity = 'go to the office'){
+
+ return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(entry = '*'){
+  const inner = function(value = "special") {
+  return `You are ${entry}${value}${entry}!`
+  }
+  return inner;
+  }
